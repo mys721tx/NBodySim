8e3186e2b038e25605aeb38100ac2173ffdc55e7
Yishen Miao <mys721tx@gmail.com>
2015-12-10 15:57:33 -0800

    Refactoring
    Refactors NBodySim.py.
    
    	modified:   NBodySim.py

--- NBodySim.py
+++ NBodySim.py
@@ -2,49 +2,82 @@
 NBodySim.py: a simple n-body simulator
 """
 
+import itertools
 import numpy
 import scipy.constants
-import itertools
 import matplotlib.pyplot
 
-def gravity(m1, m2, r):
-    return scipy.constants.G * m1 * m2 / (r ** 2)
+def gravity(mass_1, mass_2, radius):
+    """
+    calculate the gravtional attraction between m1 and m2.
+    """
 
-positions = numpy.array([[-50, -50], [-50, 50], [50, 50], [50, -50]], dtype = numpy.float64)
+    return scipy.constants.G * mass_1 * mass_2 / (radius ** 2)
 
-velocities = numpy.array([[0, 5], [5, 0], [0, -5], [-5, 0]], dtype = numpy.float64)
+def particle_to_particle():
+    """
+    the particale to particle simulator
+    """
 
-#accelerations = numpy.array([[0, 0], [0, 0], [0, 0], [0, 0]], dtype = numpy.float64)
+    positions = numpy.array([
+        [-50, -50],
+        [-50, 50],
+        [50, 50],
+        [50, -50]], dtype=numpy.float64)
 
-masses = numpy.array([100000000000, 100000000000, 100000000000, 100000000000], dtype = numpy.float64)
+    velocities = numpy.array([
+        [0, 5],
+        [5, 0],
+        [0, -5],
+        [-5, 0]], dtype=numpy.float64)
 
-d_t = 1
+    #accelerations = numpy.array([[0, 0], [0, 0], [0, 0], [0, 0]], dtype = numpy.float64)
 
-fig, ax = matplotlib.pyplot.subplots()
-ax.set_xlim(-1000, 1000)
-ax.set_ylim(-1000, 1000)
+    masses = numpy.array(
+        [100000000000, 100000000000, 100000000000, 100000000000],
+        dtype=numpy.float64)
 
-points, = ax.plot(positions[:, 0], positions[:, 1], marker='o', linestyle='None')
+    d_t = 1
 
-for i in range(500):
-    
-    accelerations = numpy.array([[0, 0], [0, 0], [0, 0], [0, 0]], dtype = numpy.float64)
+    fig, axis = matplotlib.pyplot.subplots()
+    axis.set_xlim(-1000, 1000)
+    axis.set_ylim(-1000, 1000)
 
-    for pair in itertools.combinations(range(len(masses)), 2):
-        particle_a, particle_b = pair
-        d = positions[particle_b] - positions[particle_a]
+    points, = axis.plot(positions[:, 0], positions[:, 1], marker='o', linestyle='None')
 
-        r = numpy.linalg.norm(d)
-    
-        f = gravity(masses[particle_a], masses[particle_b], r)
+    for i in range(2):
 
-        accelerations[particle_a] += f / masses[particle_a] * d
-        accelerations[particle_b] -= f / masses[particle_b] * d
+        accelerations = numpy.array([
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0]], dtype=numpy.float64)
 
-    velocities += accelerations * d_t
+        for pair in itertools.combinations(range(len(masses)), 2):
+            particle_a, particle_b = pair
+            direction = positions[particle_b] - positions[particle_a]
 
-    positions += 0.5 * accelerations * d_t ** 2 + velocities * d_t
+            radius = numpy.linalg.norm(direction)
 
-    points.set_data(positions[:, 0], positions[:, 1])
-    
-    matplotlib.pyplot.savefig("%03d.png" % i)
+            force = gravity(masses[particle_a], masses[particle_b], radius)
+
+            accelerations[particle_a] += force / masses[particle_a] * direction
+            accelerations[particle_b] -= force / masses[particle_b] * direction
+
+        velocities += accelerations * d_t
+
+        positions += 0.5 * accelerations * d_t ** 2 + velocities * d_t
+
+        points.set_data(positions[:, 0], positions[:, 1])
+
+        matplotlib.pyplot.savefig("%03d.png" % i)
+
+def main():
+    """
+    the simulator
+    """
+
+    particle_to_particle()
+
+if __name__ == "__main__":
+    main()
